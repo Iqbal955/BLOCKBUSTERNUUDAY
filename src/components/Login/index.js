5b7dd2406fa8a7395bb4ea932a26f1af97fea00e
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2022-03-17 15:46:53 +0100

    home button working now

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,45 +1,46 @@
 import { Link } from "react-router-dom";
 import {
-    Container,
-    Content,
-    CTA,
-    CTALogoOne,
-    SignUp,
-    Description,
-    CTALogoTwo,
-    BgImage,
+  Container,
+  Content,
+  CTA,
+  CTALogoOne,
+  SignUp,
+  Description,
+  CTALogoTwo,
+  BgImage,
 } from "./StyledLogoElements";
 
-{/*
+{
+  /*
 //The login component is a stateless component. It does not have any state. It only renders the login form. The login form is a stateless component.
  It does not have any state. It only renders the login form. 
 
-*/}
-
+*/
+}
 
 const Login = () => {
-    return (
-        <Container>
-            <Content>
-                <CTA>
-                    <CTALogoOne src="https://theme.zdassets.com/theme_assets/1092563/a245708927603119f60e2b304894e844a57d6839.png" alt=""/>
-                    <SignUp>UDFORSK BLOCKBUSTER UNIVERSET</SignUp>
-                    <Description>
-                     DE FEDESTE FILM, DE FEDESTE SERIER, 
-                  
-                     DEN FEDESTE KINO
-                    </Description>
-                    <Link to="/movies"> 
-                    
-                    <CTALogoTwo src="https://www.pikpng.com/pngl/b/197-1971500_find-the-best-film-tv-and-video-production.png" />
-                    </Link>
-                   
-                   
-                </CTA>
-                <BgImage />
-            </Content>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne
+            src="https://theme.zdassets.com/theme_assets/1092563/a245708927603119f60e2b304894e844a57d6839.png"
+            alt=""
+          />
+          <SignUp>
+            <Link to="/home">UDFORSK BLOCKBUSTER UNIVERSET</Link>
+          </SignUp>
+
+          <Description>
+            DE FEDESTE FILM, DE FEDESTE SERIER, DEN FEDESTE KINO
+          </Description>
+
+          <CTALogoTwo src="https://www.pikpng.com/pngl/b/197-1971500_find-the-best-film-tv-and-video-production.png" />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
 
 export default Login;
